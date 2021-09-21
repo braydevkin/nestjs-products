ccfa7215811c67464ce49a12538304495f4f61b7
braydevkin <brayan.molk5@gmail.com>
2021-09-21 11:42:25 -0300

    Add CORS configurations

---
+++ src/configs/constants.ts
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT;
+
+export const CORS_WHITE_LIST = ['localhost'];
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { CORS_WHITE_LIST, PORT } from './configs/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,25 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  app.enableCors({
+    origin: function(origin, callback) {
+      if (!origin || CORS_WHITE_LIST.some(domain => origin.includes(domain))) {
+        callback(null, true);
+      } else {
+        console.warn(`CORS: Blocking from origin ${origin}`);
+        callback(null, false);
+      }
+    },
+    credentials: true,
+  });
+
+  console.log(`Server running on the port ${PORT}`);
+  console.log(`The documentation is acessible in /docs`);
+  console.log(`CORS configured to accept request from: `);
+  CORS_WHITE_LIST.forEach(origin => {
+    console.log(`- ${origin}`);
+  });
+
+  await app.listen(PORT);
 }
 bootstrap();
